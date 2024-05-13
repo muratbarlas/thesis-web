9557b52db416bd146e99f8db22ee123681624bcb
muratbarlas <47250734+muratbarlas@users.noreply.github.com>
2024-05-12 22:51:10 -0400

    gpt csv writer added

--- gpt_headline_generator.py
+++ gpt_headline_generator.py
@@ -3,21 +3,25 @@ from openai import OpenAI
 import os
 import csv
 load_dotenv('.env')
-api_key = os.getenv('api_key')
-print(api_key)
-# client = OpenAI(api_key= api_key_)
-#
-#
-# with open('fox_gpt.csv', 'w', newline='') as file:
-#   writer = csv.writer(file)
-#   for i in range(50):
-#     completion = client.chat.completions.create(
-#       model=model_,
-#       messages=[
-#         {"role": "system", "content": "You are an assistant that generates news headlines"},
-#         {"role": "user", "content": "Generate a news headline"}
-#       ]
-#     )
-#     reply = completion.choices[0].message.content
-#     writer.writerow([reply])
-#     print(reply)
+api_key_ = os.getenv('api_key')
+reddit_model = os.getenv('reddit_model')
+fox_model =  os.getenv('fx_news_model')
+
+
+
+client = OpenAI(api_key= api_key_)
+
+
+with open('reddit_gpt.csv', 'w', newline='') as file:
+  writer = csv.writer(file)
+  for i in range(100):
+    completion = client.chat.completions.create(
+      model=reddit_model,
+      messages=[
+        {"role": "system", "content": "You are an assistant that generates news headlines"},
+        {"role": "user", "content": "Generate a news headline"}
+      ]
+    )
+    reply = completion.choices[0].message.content
+    writer.writerow([reply])
+    print(reply)
